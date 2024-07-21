1a1395f6a42a32de0cfdb3cd92f45dbafde46d31
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-21 14:38:41 -0300

    feat: add post user in page Register

--- frontend/src/routes/register/RegisterPage.jsx
+++ frontend/src/routes/register/RegisterPage.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './registerPage.scss';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./registerPage.scss";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8800/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        }
+      );
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+      setError(error.response.data.message);
+    }
+  };
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Criar uma Conta</h1>
           <input name="username" type="text" placeholder="Usuário" />
           <input name="email" type="text" placeholder="E-mail" />
           <input name="password" type="password" placeholder="Senha" />
           <button>Registrar</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Já tem uma conta?</Link>
         </form>
       </div>
@@ -20,6 +47,6 @@ const RegisterPage = () => {
       </div>
     </div>
   );
-}
+};
 
 export default RegisterPage;
