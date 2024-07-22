d8c7f193f79a8c55ffe784931749d6890d5d0e15
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-21 23:30:07 -0300

    feat: add post image and add react cloudinary for storage images

--- frontend/src/routes/profileUpdatePage/ProfileUpdatePage.jsx
+++ frontend/src/routes/profileUpdatePage/ProfileUpdatePage.jsx
@@ -3,6 +3,7 @@ import "./profileUpdatePage.scss";
 import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../lib/apiRequest";
 import { useNavigate } from "react-router-dom";
+import UploadWidget from '../../components/uploadWidget/UploadWidget';
 
 const ProfileUpdatePage = () => {
     const { currentUser, updateUser } = useContext(AuthContext);
@@ -20,6 +21,7 @@ const ProfileUpdatePage = () => {
           username,
           email,
           password,
+          avatar:avatar[0]
         })
         updateUser(response.data)
         navigate("/profile");
@@ -63,6 +65,14 @@ const ProfileUpdatePage = () => {
         </div>
         <div className="sideContainer">
           <img src={avatar[0] || currentUser.avatar || "/noavatar.jpg"} alt="" className="avatar" />
+          <UploadWidget uwConfig={
+            {
+              cloudName:"lucasdiello",
+              uploadPreset:"ld_homes",
+              multiple: false,
+              maxImageFileSize: 2000000,
+              folder: "avatars"
+            }} setState={setAvatar} />
         </div>
       </div>
     );
