76120a81db7b891b63d839bc8753e4ab8ff9aaa9
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-23 18:05:15 -0300

    feat: add post.route & controller & service

--- api/app.js
+++ api/app.js
@@ -14,6 +14,7 @@ console.log("test");
 app.use('/api/posts', postRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(8800, () => {
     console.log('Server is running on port 8800');
