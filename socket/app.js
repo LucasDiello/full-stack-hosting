7895a4d72fe001e139f1d9b8c71184a47cd3c313
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-25 14:51:42 -0300

    feat: increment logic socket.io

--- socket/app.js
+++ socket/app.js
@@ -1,17 +1,49 @@
-import { Server } from 'socket.io'
+import { Server } from "socket.io";
 
 const io = new Server({
-    cors: {
-        origin: "http://localhost:5173",
-    },
-})
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
+});
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg)
-    }   )
-})
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+    const userExits = onlineUser.find((user) => user.userId === userId);
+    if (!userExits) {
+      onlineUser.push({ userId, socketId });
+    }
+};
+
+const removeUser = (socketId) => {
+  return onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+ 
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (!receiver) {
+      return console.log("User not found");
+    }
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+io.listen(4000);
