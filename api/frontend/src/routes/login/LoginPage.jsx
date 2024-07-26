51703ed932dec514e1af7da7f61d56eacf506e2a
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-26 18:52:59 -0300

    fix: style login & register

--- api/frontend/src/routes/login/LoginPage.jsx
+++ api/frontend/src/routes/login/LoginPage.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import './loginPage.scss';
-import apiRequest from '../../lib/apiRequest';
-import { AuthContext } from '../../context/AuthContext';
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import "./loginPage.scss";
+import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../context/AuthContext";
+import { SlLogin } from "react-icons/sl";
 
 const LoginPage = () => {
   const [error, setError] = useState("");
@@ -16,19 +17,14 @@ const LoginPage = () => {
     setError("");
     const formData = new FormData(e.target);
     const { username, password } = Object.fromEntries(formData);
-    //const username = formData.get("username");
-    //const password = formData.get("password");
 
     try {
-      const response = await apiRequest.post(
-        "/auth/login",
-        {
-          username,
-          password,
-        }
-      );
+      const response = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
       updateUser(response.data);
-      navigate("/"); 
+      navigate("/");
     } catch (error) {
       console.error(error);
       setError(error.response.data.message);
@@ -38,17 +34,26 @@ const LoginPage = () => {
   };
   return (
     <div className="login">
-            <div className={`${pathname === "/login" && "before"}`}/>
-
+      <div className={`${pathname === "/login" && "before"}`} />
       <div className="formContainer">
         <form onSubmit={handleSubmit}>
-          <h1>Bem-vindo de volta</h1>
-          <input name="username" required minLength={3} maxLength={20} type="text" placeholder="Usuário" />
+          <h1>Seja Bem-vindo</h1>
+          <p>
+            Entre com seu usuário e senha para acessar a plataforma e encontre seu imóvel. 
+             Aqui você pode encontrar o imóvel dos seus sonhos. 
+          </p>
+          <input
+            name="username"
+            required
+            minLength={3}
+            maxLength={20}
+            type="text"
+            placeholder="Usuário"
+          />
           <input name="password" required type="password" placeholder="Senha" />
           {error && <span>{error}</span>}
-          <button disabled={
-            isLoading
-          }>Entrar</button>
+          <button disabled={isLoading}><SlLogin size={20}/>
+          </button>
           <Link to="/register">Ainda não tem uma conta?</Link>
         </form>
       </div>
@@ -57,6 +62,6 @@ const LoginPage = () => {
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
