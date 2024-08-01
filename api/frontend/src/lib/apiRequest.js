e63c1ed9301e9a23a5bbfe3f7996137035960c90
LucasDiello <114077422+LucasDiello@users.noreply.github.com>
2024-08-01 13:30:13 -0300

    Update apiRequest.js

--- api/frontend/src/lib/apiRequest.js
+++ api/frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-    baseURL: 'https://aluguejahotel.vercel.app',
+    baseURL: 'https://aluguejahotel.onrender.com/api',
     withCredentials: true,
 })
 
