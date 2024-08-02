9eae9eb895680dd59d584a1597429ea31d7d01ad
Lucas <lucasoliveiradiello@hotmail.com>
2024-08-02 19:35:43 -0300

    fix: test create icon to marker in map but he was conflict

--- api/frontend/src/components/pin/Pin.jsx
+++ api/frontend/src/components/pin/Pin.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Marker, Popup } from 'react-leaflet'
 import { Link } from 'react-router-dom'
+import { Icon } from 'leaflet'
 import './pin.scss'
+import icon from "../../../public/pinMap.png"
+
+const myIcon = new Icon({
+  iconUrl: icon,
+  iconSize: [32,32]
+ })
+
 
 const Pin = ({ item }) => {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker icon={myIcon} position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
           <img src={item.images[0]} alt="" />
