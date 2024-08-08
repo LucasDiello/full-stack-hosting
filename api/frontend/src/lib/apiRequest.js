5bbd2341eef326fbf7f0396bc4eeaac5b343ab81
LucasDiello <114077422+LucasDiello@users.noreply.github.com>
2024-08-08 17:22:38 -0300

    Update apiRequest.js

--- api/frontend/src/lib/apiRequest.js
+++ api/frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://alugueja.onrender.com/api',
     withCredentials: true,
 })
 
