800126964082f9ed5de41d4c615090c66c5c604b
Lucas Diello <lucasoliveiradiello@gmail.com>
2024-08-12 18:30:38 -0300

    feat: transfer folder to best deploy

--- api/app.js
+++ api/app.js
@@ -2,18 +2,19 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import { authRoute, chatRoute, messageRoute, postRoute, userRoute }  from './routes/index.js';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 4000
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 
 app.use('/api/posts', postRoute)
 app.use('/api/auth', authRoute)
@@ -23,8 +24,6 @@ app.use('/api/chats', chatRoute)
 app.use('/api/messages', messageRoute)
 app.use('/test', userRoute)
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/dist/index.html')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
