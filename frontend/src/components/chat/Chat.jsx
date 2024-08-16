08f9d168ff0225aefe19d4b5a72b20924fc2bc8e
Lucas Diello <lucasoliveiradiello@gmail.com>
2024-08-16 14:48:56 -0300

    feat: increment new css to chat & remove props because dont att when sending a message

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -5,12 +5,34 @@ import apiRequest from "../../lib/apiRequest";
 import { format } from "timeago.js";
 import { SocketContext } from "../../context/SocketContext";
 import { useNotificationStore } from "../../lib/notificationStore";
-
-function Chat({ chats }) {
+import { HiChatBubbleOvalLeft } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+function Chat() {
   const [chat, setChat] = useState(null);
+  const [upd, setUpd] = useState([]);
+  const [open, setOpen] = useState(false);
   const { currentUser } = useContext(AuthContext);
   const { socket } = useContext(SocketContext);
+
+  const messageEndRef = useRef();
+
   const decrease = useNotificationStore((state) => state.decrease);
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chat]);
+
+  const fetchChats = async () => {
+    try {
+      const res = await apiRequest("/chats");
+      setUpd(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const handleOpenChat = async (id, receiver) => {
     try {
       const res = await apiRequest("/chats/" + id);
@@ -23,17 +45,23 @@ function Chat({ chats }) {
     }
   };
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    
     const formData = new FormData(e.target);
     const text = formData.get("text");
-
+    
     if (!text) return;
     try {
       const res = await apiRequest.post("/messages/" + chat.id, { text });
       setChat((prev) => ({ ...prev, messages: [...prev.messages, res.data] }));
+      
+      const updatedChats = await apiRequest("/chats");
+      setUpd(updatedChats.data);
+      
       e.target.reset();
+      
       socket.emit("sendMessage", {
         receiverId: chat.receiver.id,
         data: res.data,
@@ -42,8 +70,10 @@ function Chat({ chats }) {
       console.log(err);
     }
   };
-
+  
   useEffect(() => {
+    fetchChats();
+    
     const read = async () => {
       try {
         await apiRequest.put("/chats/read/" + chat.id);
@@ -51,47 +81,66 @@ function Chat({ chats }) {
         console.log(err);
       }
     };
-
+    
     if (chat && socket) {
       socket.on("getMessage", (data) => {
-        console.log(data)
+        console.log(data);
         if (chat.id === data.chatId) {
+          console.log("entrei");
           setChat((prev) => ({ ...prev, messages: [...prev.messages, data] }));
+          console.log("entrei2");
           read();
         }
       });
     }
     return () => {
-      socket.off("getMessage");
+      if (socket) return socket.off("getMessage");
     };
   }, [socket, chat]);
-  console.log(chats[1].lastMessage)
+  
+  if (currentUser) fetch();
+
   return (
     <div className="chat">
       <div className="messages">
-        <h1>Mensagens</h1>
-        {chats?.map((c) => (
-          <div
-            className="message"
-            key={c.id}
-            style={{
-              backgroundColor:
-                c.seenBy.includes(currentUser.id) || chat?.id === c.id
-                  ? "white"
-                  : "#fecd514e",
-            }}
-            onClick={() => handleOpenChat(c.id, c.receiver)}
-          >
-            <img src={c.receiver.avatar || "/noavatar.jpg"} alt="" />
-            <div>
-              <span>{c.receiver.username}</span>
-              <p>{c.lastMessage}</p>
-            </div>
+        <div className="box" onClick={() => setOpen(!open)}>
+          <img src={currentUser.avatar || "./noavatar.jpg"} alt="" />
+          <h1>Mensagens</h1>
+          <p>
+            <HiChatBubbleOvalLeft size={30} />
+            <span>{number}</span>
+          </p>
+        </div>
+        {
+          <div className={`messages-list ${open ? "active " : ""}`}>
+            {upd?.map((c) => (
+              <div
+                className="message"
+                key={c.id}
+                style={{
+                  backgroundColor:
+                    c.seenBy.includes(currentUser.id) || chat?.id === c.id
+                      ? "white"
+                      : "#fecd514e",
+                }}
+                onClick={() => handleOpenChat(c.id, c.receiver)}
+              >
+                <img src={c.receiver.avatar || "/noavatar.jpg"} alt="" />
+                <div>
+                  <span>{c.receiver.username}</span>
+                  <p>
+                    {c.lastMessage.length > 10
+                      ? c.lastMessage.substring(0, 10) + "..."
+                      : c.lastMessage}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        }
       </div>
       {chat && (
-        <div className="chatBox">
+        <div className={`chatBox ${chat ? "active2" : ""}`}>
           <div className="top">
             <div className="user">
               <img src={chat.receiver.avatar || "noavatar.jpg"} alt="" />
@@ -114,6 +163,7 @@ function Chat({ chats }) {
                 <p>{message.text}</p>
               </div>
             ))}
+            <div ref={messageEndRef}></div>
           </div>
           <form onSubmit={handleSubmit} className="bottom">
             <textarea name="text"></textarea>
