8668a425e9955f2438d61a2a6cb93328911c93a7
Lucas Diello <lucasoliveiradiello@gmail.com>
2024-08-21 10:25:34 -0300

    feat: increment emit userOnline to render in chatComponent

--- socket/app.js
+++ socket/app.js
@@ -11,19 +11,23 @@ const io = new Server({
   },
 });
 
-
 let onlineUser = [];
 
 const addUser = (userId, socketId) => {
-  const userExits = onlineUser.find((user) => user.userId === userId);
-  if (!userExits) {
-    console.log("User added");
-    onlineUser.push({ userId, socketId, online: true, lastSeen: new Date() });
+  const userExists = onlineUser.find((user) => user.userId === userId);
+  if (!userExists) {
+    onlineUser.push({ userId, socketId, lastSeen: new Date() });
+    io.emit("userOnline", onlineUser);
+  } else {
+    userExists.socketId = socketId;
+    userExists.lastSeen = new Date();
+    io.emit("userOnline", onlineUser); 
   }
 };
 
 const removeUser = (socketId) => {
-  return onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+  io.emit("userOnline", onlineUser); 
 };
 
 const getUser = (userId) => {
@@ -32,23 +36,24 @@ const getUser = (userId) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
   socket.on("newUser", (userId) => {
     addUser(userId, socket.id);
+    socket.emit("userOnline", onlineUser); 
   });
-  
+
   socket.on("sendMessage", ({ receiverId, data }) => {
     const receiver = getUser(receiverId);
-    if (!receiver) {
-      return console.log("User not found");
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    } else {
+      console.log("User not found");
     }
-    io.to(receiver.socketId).emit("getMessage", data);
   });
-  
 
   socket.on("disconnect", () => {
-    console.log("a user disconnected");
     removeUser(socket.id);
   });
-}); 
+});
 
 io.listen(4001);
