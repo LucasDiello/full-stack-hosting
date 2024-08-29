61c6e6a4b8b109b8681030af5e902d5390fbeadf
LucasDiello <lucasoliveiradiello@hotmail.com>
2024-08-29 13:12:10 -0300

    feat: increment buttonf for view more items list

--- frontend/src/routes/listPage/ListPage.jsx
+++ frontend/src/routes/listPage/ListPage.jsx
@@ -1,22 +1,37 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
-import { Await, useLoaderData } from "react-router-dom";
+import { Await, useLoaderData, useNavigate, useSearchParams } from "react-router-dom";
 import { CiFaceFrown } from "react-icons/ci";
+import apiRequest from "../../lib/apiRequest";
+import { IoIosArrowDown } from "react-icons/io";
 const ListPage = () => {
-  
+  const [searchParams, setSearchParams] = useSearchParams();
   const data = useLoaderData();
-  console.log(data);
-  console.log(data.postResponse.data);
+  const [take, setTake] = useState(3);
+  const [posts, setPosts] = useState(data.postResponse);
+  useEffect(() => {
+    setPosts(data.postResponse);
+  } , [searchParams]);
+
+  const handleMorePosts = async () => {
+    setTake((prev) => prev + 3);
+    // Atualize o parâmetro morePosts na URL com o novo valor
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set('morePosts', take);
+  
+    setSearchParams(newParams);
+  };
+  console.log(searchParams.toString());
   return (
     <div className="listPage">
       <div className="listContainer">
           <Filter />
         <div className="wrapper">
           {
-            data.postResponse.data.length === 0 ? <div className="post-notfound">
+            posts.data.length === 0 ? <div className="post-notfound">
               <p>
               Infelizmente não encontramos nenhum imóvel com essas características.
               </p>
@@ -24,7 +39,7 @@ const ListPage = () => {
               </div> :
           <Suspense fallback={<p>Loading...</p>}>
             <Await
-              resolve={data.postResponse}
+              resolve={posts}
               errorElement={<p>Error loading posts!</p>}
             >
               {(postResponse) =>
@@ -36,11 +51,16 @@ const ListPage = () => {
           </Suspense>
           }
         </div>
+        <div className="btn">
+            <button onClick={handleMorePosts} className="btn">
+              <IoIosArrowDown size={20} />
+                </button>
+        </div>
       </div>
       <div className="mapContainer">
         <Suspense fallback={<p>Loading...</p>}>
           <Await
-            resolve={data.postResponse}
+            resolve={posts}
             errorElement={<p>Error loading posts!</p>}
           >
             {(postResponse) => <Map items={postResponse.data} />}
